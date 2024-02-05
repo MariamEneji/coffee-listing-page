ce5f15f03a25105d77d6be6e748b65fec7a440b9
Mariam Eneji <enejimariam1997@gmail.com>
2024-02-05 11:30:47 +0100

    update

--- src/components/Coffee.jsx
+++ src/components/Coffee.jsx
@@ -27,16 +27,16 @@ const filteredCoffeeData = available ? coffeeData?.filter((coffee) => coffee.ava
       <div className='h-[45%] md:h-[25%] w-full backg absolute'></div>
 
       <div className='z-10 mx-12 md:mx-20 my-28 px-10 md:px-10 py-20 md:py-28 bg-newGrey h-full rounded-xl flex flex-col items-center text-center '>
-        <img src={curl} alt="" className='absolute -right-4 md:right-[23.5rem] top-36  md:top-44' />
-        <h1 className='text-newWhite text-[2rem] py-4 z-20 '>Our Collection</h1>
+        <img src={curl} alt="" className='absolute -right-4 md:right-[13rem] lg:right-[23.5rem] top-36  lg:top-44 md:top-40' />
+        <h1 className='text-newWhite text-[2rem]  z-20 font-semibold '>Our Collection</h1>
         <p className='text-newBlue lg:px-60 md:px-10 z-20'>
           Introducing our Coffee Collection, a selection of unique coffees from different roast types and origins,
           expertly roasted in small batches and shipped fresh weekly.
         </p>
 
-        <div className='py-4'>
-          <button onClick={handleAll} className={!available ? 'bg-newBlue m-2 md:4 p-2 rounded-xl text-newWhite ' : 'm-2 md:m-4 p-2 text-newWhite'}>All Products</button>
-          <button onClick={handleAvailable} className={available ? 'bg-newBlue m-2 md:4 p-2 rounded-xl text-newWhite ' : 'm-2 md:m-4 p-2 text-newWhite'}>Available now</button>
+        <div className='pb-4'>
+          <button onClick={handleAll} className={!available ? 'bg-newBlue m-2  px-4 py-2 rounded-xl text-newWhite ' : 'm-2  px-4 py-2   text-newWhite'}>All Products</button>
+          <button onClick={handleAvailable} className={available ? 'bg-newBlue m-2 px-4 py-2 rounded-xl text-newWhite ' : 'm-2   text-newWhite px-4 py-2 '}>Available now</button>
         </div>
 
         {coffeeData ? (
