55a3867a60e6f9eed256541058320aa081e18d48
Mariam Eneji <enejimariam1997@gmail.com>
2024-02-06 10:19:16 +0100

    update

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -26,16 +26,16 @@ const Info = () => {
     <div>
       {coffeeInfo && (
 
-        <div className='w-full bg-newBlack h-screen back flex flex-col md:flex-row justify-center  items-center gap-20 px-20'>
+        <div className='w-full bg-newBlack h-screen back flex flex-col md:flex-row justify-center  items-center gap-10 md:gap-20 px-20'>
 
-            <div className='rounded-xl w-1/2 z-10'>
+            <div className='rounded-xl mt-30 w-full md:w-1/2 z-10'>
                 <img src={coffeeInfo.image} alt="" className='rounded-xl w-full'/>
 
             </div>
 
-         <div className='w-1/2 flex flex-col justify-center items-center z-10 text-newWhite gap-4'> 
-         <h1 className='text-5xl text-newYellow'>{coffeeInfo.name}</h1>
-          <p className='text-3xl'>{coffeeInfo.price}</p>
+         <div className='md:w-1/2 flex flex-col justify-center items-center z-10 text-newWhite lg:gap-2'> 
+         <h1 className='text-4xl lg:text-5xl text-newYellow'>{coffeeInfo.name}</h1>
+          <p className='text-2xl lg:3xl'>{coffeeInfo.price}</p>
           <p>This coffee is rated {coffeeInfo.rating ? coffeeInfo.rating : 0}</p>
           <p className='text-newYellow'>{coffeeInfo.available ? 'Place and order for this right now' : 'This coffee is not available right now'}</p>
          <Link to='/' className='border-newPink border px-4 py-2 rounded-full text-newPink hover:bg-newPink hover:text-newWhite'>Back Home</Link>
